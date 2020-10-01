725d5c276837fb70cd63d847545485e0c988236e
natalialexandra <natalia.alexandra195@gmail.com>
2020-10-01 11:06:57 +0200

    change icons

--- src/App.js
+++ src/App.js
@@ -1,18 +1,17 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
 import List from './components/List';
-// import AddItem from './components/AddItem';
+import { RiSendPlaneFill } from 'react-icons/ri';
+import './App.css';
+
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        <RiSendPlaneFill className="App-logo" />
       </header>
 
       <List />
-
     </div>
   );
 }
