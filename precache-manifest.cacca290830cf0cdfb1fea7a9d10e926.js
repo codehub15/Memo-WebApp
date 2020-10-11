17503ac9d3190094fbfb8286252e54d164195e8a
natalialexandra <natalia.alexandra195@gmail.com>
2020-10-11 10:00:54 +0200

    Updates

---
+++ precache-manifest.cacca290830cf0cdfb1fea7a9d10e926.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b7d42fb0a7ed65da4226f54a5bcf7306",
+    "url": "./index.html"
+  },
+  {
+    "revision": "87a3b21e0359fc70c9d0",
+    "url": "./static/css/main.75401557.chunk.css"
+  },
+  {
+    "revision": "982c6fc15ebcd26b6139",
+    "url": "./static/js/2.886bfd10.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "./static/js/2.886bfd10.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "87a3b21e0359fc70c9d0",
+    "url": "./static/js/main.68903234.chunk.js"
+  },
+  {
+    "revision": "79aed1c88c05a1d50bd2",
+    "url": "./static/js/runtime-main.ccd662ec.js"
+  }
+]);
